c7da34b718eea34e07263740f654d9e334a569ae
Md Minhaaz <minhaaz52@gmail.com>
2024-02-29 04:18:50 +0530

    edit functionality added

---
+++ src/components/EditTask.vue
@@ -0,0 +1,130 @@
+<script setup>
+import { ref, reactive, onMounted } from "vue"
+import { doc, setDoc, collection, getDoc, updateDoc } from "firebase/firestore";
+import { db } from "@/firebase"
+
+const data = reactive({
+    statusItems: [
+        { name: "To Do", id: "To Do" },
+        { name: "In Progress", id: "In Progress" },
+        { name: "Done", id: "Done" }
+    ],
+
+    selectedStatus: "To Do",
+    name: "",
+    description: "",
+    btnLoading: false,
+    preLoader: true,
+})
+
+const createTaskForm = ref(null)
+
+const emits = defineEmits(['closeDialogEmit'])
+const props=defineProps(['itemId'])
+
+const nameRules = [v => !!v || 'Name is required']
+const descriptionRules = [v => !!v || 'Description is required']
+const statusRules = [v => !!v || "Status is required"]
+
+
+onMounted(async()=>{
+    try{
+        await getItemData();
+        data.preLoader=false;
+    } catch(err){
+        console.log(err);
+        data.preLoader=false;
+        alert("Something went wrong")
+    }
+})
+
+const closeDialog = () => {
+    emits('closeDialogEmit')
+}
+
+const getItemData=async()=>{
+    try{
+        const docRef=doc(db, "tasks", props.itemId);
+        const docSnap=await getDoc(docRef);
+
+        if (docSnap.exists()){
+            const temp=docSnap.data();
+            data.name=temp.name;
+            data.description=temp.description
+            data.selectedStatus=temp.status;
+            return;
+        }
+        closeDialog();
+        alert("Document not found");
+    } catch(err){
+        console.log(err);
+        alert("Something went wrong");
+    }
+
+}
+
+const submitForm = async () => {
+    try{
+        data.btnLoading=true;
+        const validate = await createTaskForm.value.validate();
+
+        if (!validate.valid){
+            data.btnLoading=false;
+            return;
+        }
+
+        const date=new Date();
+        const taskRef=doc(db, "tasks", props.itemId);
+
+        await updateDoc(taskRef, {
+            name: data.name,
+            description: data.description,
+            status: data.selectedStatus,
+            updatedOnOn: date.toString(),
+        })
+        closeDialog();
+        data.btnLoading=false;
+    } catch(err){
+        console.log(err);
+        data.btnLoading=false;
+        alert("Something went wrong");
+    }
+}
+
+</script>
+
+
+<template>
+    <v-form class="d-flex flex-column justify-center align-center w-100 h-100 pa-2" @submit.prevent="submitForm"
+        ref="createTaskForm">
+        <v-card class="pa-4 w-50">
+            <div v-if="data.preLoader" class="h-100 w-100">
+                <v-skeleton-loader type="card"></v-skeleton-loader>
+            </div>
+            <div v-else>
+            <div class="d-flex align-center mb-2">
+                <v-icon size="medium">mdi-pencil</v-icon>
+                <h4>Edit Task</h4>
+            </div>
+            <v-text-field v-model="data.name" class="mb-4 mr-4" required hide-details="auto" label="Task Name" :rules="nameRules"
+                variant="outlined"></v-text-field>
+
+            <v-text-field v-model="data.description" class="mb-4 mr-4" required hide-details="auto" label="Task Description" :rules="descriptionRules"
+                variant="outlined"></v-text-field>
+
+            <v-autocomplete v-model="data.selectedStatus" label="Status" required class="mr-4" :items="data.statusItems"
+                item-title="name" item-value="id" :rules="statusRules" variant="outlined"></v-autocomplete>
+
+            <div class="">
+                <v-btn :loading="data.btnLoading" type="submit" variant="outlined" color="deep-purple-accent-4" class="w-100 pa-2 mb-4 rounded-xl">
+                    Update
+                </v-btn>
+
+                <v-btn @click="closeDialog" variant="outlined" color="deep-purple-accent-4" class="w-100 mb-4 rounded-xl">
+                    Cancel
+                </v-btn>
+            </div>
+            </div>
+        </v-card>
+    </v-form>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,14 +4,14 @@ import { collection, onSnapshot, doc, deleteDoc, writeBatch } from "firebase/fir
 import { db } from "@/firebase"
 
 import CreateTask from "@/components/CreateTask.vue"
-
+import EditTask from "@/components/EditTask.vue"
 
 const data = reactive({
   items: [],
-  filterdItems:[],
+  filterdItems: [],
 
   filterStatus: [
-    {name : "All", id: "All"},
+    { name: "All", id: "All" },
     { name: "To Do", id: "To Do" },
     { name: "In Progress", id: "In Progress" },
     { name: "Done", id: "Done" }
@@ -20,70 +20,128 @@ const data = reactive({
   selectedFilter: "All",
   search: "",
   dialog: false,
-  selectedItems:[],
+  selectedItems: [],
+  editDialog: false,
+  selectedItem:"",
+  deleteLoading: false,
 })
 
 watch(
-  ()=>data.search,
-  ()=>{
+  () => data.search,
+  () => {
     getFilteredData();
   }
 )
 
 watch(
-  ()=>data.selectedFilter,
-  ()=>{
+  () => data.selectedFilter,
+  () => {
     getFilteredData();
   }
 )
 
 const headers = [
-    { title: 'Name', align: 'start', sortable: true, key: 'name' },
-    { title: 'Description', align: 'center', sortable: true, key: 'description' },
-    { title: 'Status', align: 'center', sortable: true, key: 'status' },
-    { title: 'Created On', align: 'center', sortable: true, key: 'createdOn' },
-    { title: 'Updated On', align: 'center', sortable: true, key: 'updatedOn' },
-    { title: 'Actions', align: 'center', key: 'actions', sortable: false },
+  { title: 'Name', align: 'start', sortable: true, key: 'name' },
+  { title: 'Description', align: 'center', sortable: true, key: 'description' },
+  { title: 'Status', align: 'center', sortable: true, key: 'status' },
+  { title: 'Created On', align: 'center', sortable: true, key: 'createdOn' },
+  { title: 'Updated On', align: 'center', sortable: true, key: 'updatedOn' },
+  { title: 'Actions', align: 'center', key: 'actions', sortable: false },
 ]
 
-const closeAddTaskDialog=()=>{
-  data.dialog=false;
-}
-
-const getFilteredData=()=>{
-  data.filterdItems.splice(0)
-  for (const item of data.items){
-    if (item.name?.toLowerCase()?.includes(data.search) && (data.selectedFilter==='All'|| data.selectedFilter===item.status)){
-      data.filterdItems.push(item);
-    }
-  }
-}
-
-const deleteItems=async()=>{
-  const batch = writeBatch(db);
-  for (const item of data.selectedItems){
-    const docRef=doc(db, 'tasks', item);
-    batch.delete(docRef);
-  }
-  await batch.commit();
-}
-
-const getAllData=async()=>{
-  const tasksRef=collection(db, "tasks")
-  onSnapshot(tasksRef, (querySnapshot)=>{
-    data.items.splice(0)
-    querySnapshot.forEach((task)=>{
-      data.items.push(task.data());
-    })
-    
-    getFilteredData();
-  })
-}
-
-onMounted(async()=>{
+onMounted(async () => {
   await getAllData();
 })
 
+
+const closeAddTaskDialog = () => {
+  data.dialog = false;
+}
+
+const closeEditTaskDialog=()=>{
+  data.editDialog=false;
+}
+
+
+const getFilteredData = () => {
+  try{
+    data.filterdItems.splice(0)
+    for (const item of data.items) {
+      if (item.name?.toLowerCase()?.includes(data.search) && (data.selectedFilter === 'All' || data.selectedFilter === item.status)) {
+        data.filterdItems.push(item);
+      }
+    }
+  } catch(err){
+    alert("Something went wrong")
+  }
+}
+
+const getAllData = async () => {
+  try{
+    const tasksRef = collection(db, "tasks")
+    onSnapshot(tasksRef, (querySnapshot) => {
+      data.items.splice(0)
+      querySnapshot.forEach((task) => {
+        data.items.push(task.data());
+      })
+
+      getFilteredData();
+    })
+  } catch(err){
+    alert("Something went wrong")
+  }
+}
+
+const deleteItems = async () => {
+  try{
+    data.deleteLoading=true;
+    const batch = writeBatch(db);
+    for (const item of data.selectedItems) {
+      const docRef = doc(db, 'tasks', item);
+      batch.delete(docRef);
+    }
+    await batch.commit();
+    data.deleteLoading=false;
+  } catch(err){
+    data.deleteLoading=false;
+    alert("Something went wrong");
+  }
+}
+
+
+const editItem=(item)=>{
+  data.selectedItem=item.id
+  data.editDialog=true;
+}
+
+
+
+const getItemColor=(item)=>{
+  if (item.status==="Done")
+    return "green"
+  else if (item.status==="In Progress")
+    return "orange"
+  return "red"
+}
+
+const formatTime=(time)=>{
+  if (!time)
+    return ""
+
+  const date=new Date(time);
+  if (!date)
+    return "";
+
+  const d=date.getDate().toString().padStart(2,"0")
+  const m=(date.getMonth()+1).toString().padStart(2, "0")
+  const y=date.getFullYear().toString().padStart(2,"0")
+  const h=date.getHours().toString().padStart(2, "0")
+  const mn=date.getMinutes().toString().padStart(2,"0")
+  const s=date.getSeconds().toString().padStart(2,"0")
+
+  const temp=`${d}-${m}-${y} ${h}:${mn}:${s}`
+  return temp;
+}
 
 </script>
 
@@ -93,20 +151,41 @@ onMounted(async()=>{
       <v-card width="100%" class="pa-4 mb-4 d-flex align-center justify-space-around">
         <h5 class="text-h5 font-weight-medium">Tasks</h5>
         <div class="d-flex h-25 align-center justify-space-between w-75">
-          <v-text-field v-model="data.search" class="mb-4 mr-4" hide-details="auto" label="Search by name" prepend-inner-icon="mdi-magnify"
-            variant="outlined"></v-text-field>
+          <v-text-field v-model="data.search" class="mb-4 mr-4" hide-details="auto" label="Search by name"
+            prepend-inner-icon="mdi-magnify" variant="outlined"></v-text-field>
 
-          <v-autocomplete label="Filter" class="mr-4" v-model="data.selectedFilter" :items="data.filterStatus" item-title="name"
-            item-value="id" variant="outlined"></v-autocomplete>
+          <v-autocomplete label="Filter" class="mr-4" v-model="data.selectedFilter" :items="data.filterStatus"
+            item-title="name" item-value="id" variant="outlined"></v-autocomplete>
 
-            <v-btn color="primary" class="rounded-xl mr-4" @click="deleteItems">Delete</v-btn>
-          <v-btn color="primary" class="rounded-xl" @click="data.dialog=true;">Create</v-btn>
+          <v-btn color="primary" class="rounded-xl mr-4" @click="deleteItems">Delete</v-btn>
+          <v-btn color="primary" class="rounded-xl" @click="data.dialog = true;">Create</v-btn>
 
         </div>
       </v-card>
 
       <v-card>
-        <v-data-table v-model="data.selectedItems" :headers="headers" :items="data.filterdItems" show-select></v-data-table>
+        <v-data-table v-model="data.selectedItems" :headers="headers" :items="data.filterdItems" show-select>
+
+          <template v-slot:item.actions="{ item }">
+            <v-icon size="large" class="me-2" @click="editItem(item)">
+              mdi-square-edit-outline
+            </v-icon>
+          </template>
+
+          <template v-slot:item.status="{ item }">
+            <v-chip :color="getItemColor(item)">{{ item.status }}</v-chip>
+          </template>
+
+          <template v-slot:item.createdOn="{ item }">
+            {{ formatTime(item.createdOn) }}
+          </template>
+
+          <template v-slot:item.updatedOn="{ item }">
+            {{ formatTime(item.updatedOn) }}
+          </template>
+
+
+        </v-data-table>
       </v-card>
     </div>
   </div>
@@ -114,6 +193,11 @@ onMounted(async()=>{
   <!-- Dialog for task creation -->
 
   <v-dialog v-model="data.dialog" persistent>
-    <CreateTask v-if="data.dialog" @close-dialog-emit="closeAddTaskDialog"/>
+    <CreateTask v-if="data.dialog" @close-dialog-emit="closeAddTaskDialog" />
+  </v-dialog>
+
+  <!-- Dialog for task edit -->
+  <v-dialog v-model="data.editDialog" persistent>
+    <EditTask v-if="data.editDialog" :itemId="data.selectedItem" @close-dialog-emit="closeEditTaskDialog" />
   </v-dialog>
 </template>
